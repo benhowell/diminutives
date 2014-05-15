3258859b4760da927cd6ee31d8825f57014f9a5e
benhowell <bjhowell@gmail.com>
2014-05-15 21:37:53 +1000

    new file to house functions dealing with ActionEvent stuff

--- src/main/scala/net/benhowell/diminutives/core/ActionEvents.scala
+++ src/main/scala/net/benhowell/diminutives/core/ActionEvents.scala
@@ -24,29 +24,20 @@
 
 package net.benhowell.diminutives.core
 
-import akka.actor.{Actor, Props, ActorSystem}
+import javafx.scene.control.Button
+import akka.actor.ActorRef
+import javafx.event.{ActionEvent, EventHandler}
 
 /**
- * Created by Ben Howell [ben@benhowell.net] on 22-Mar-2014.
+ * Created by Ben Howell [ben@benhowell.net] on 15-May-2014.
  */
+object ActionEvents {
 
-sealed class Subscriber(f: (String, Any) => Unit) extends Actor {
-  override def receive = { case (topic: String, payload: Any) => f(topic, payload) }
-}
-
-object EventStream{
-
-  // ActorSystem is a heavy object: create only one per application
-  // http://doc.akka.io/docs/akka/snapshot/scala/actors.html
-  val system = ActorSystem("actorsystem")
-
-  def subscribe(f: (String, Any) => Unit, name: String) = {
-    val props = Props(classOf[Subscriber], f)
-    val subscriber = system.actorOf(props, name = name)
-    system.eventStream.subscribe(subscriber, classOf[(String, Any)])
-  }
-
-  def publish(topic: String, payload: Any) {
-    system.eventStream.publish(topic, payload)
+  def setButtonOnAction(button: Button, channel: String, eventName: String, publisher: ActorRef){
+    button.setOnAction(new EventHandler[ActionEvent]() {
+      def handle(event: ActionEvent) {
+        SCEventBus.publish((channel, eventName, publisher))
+      }
+    })
   }
 }
